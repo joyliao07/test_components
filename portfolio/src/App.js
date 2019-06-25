1492dcab11a5eedbd2e23841285b2d1a76de662a
Joyce Liao <joyliao07@gmail.com>
2019-06-25 14:09:26 -0700

    config CSS module in webpack

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import classes from './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={classes.Body}>
       <header className="App-header">
         <p> Portfolio Header </p>
       </header>
