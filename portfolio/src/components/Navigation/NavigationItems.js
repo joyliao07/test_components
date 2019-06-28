c7afad7319d2a211e299a5d1a6638684bb90f080
Joyce Liao <joyliao07@gmail.com>
2019-06-27 23:49:44 -0700

    set flexbox on landing

--- portfolio/src/components/Navigation/NavigationItems.js
+++ portfolio/src/components/Navigation/NavigationItems.js
@@ -2,14 +2,18 @@ import React from 'react';
 import classes from './NavigationItems.css';
 
 const navigationItems = (props) => (
-    <ul className={classes.NavigationItems}>
-        <li><a>Home</a></li>
-        <li><a>Python</a></li>
-        <li><a>JavaScript</a></li>
-        <li><a>Algorithm</a></li>
-        <li><a>About</a></li>
-        <li><a>Contact</a></li>
-    </ul>
+    <div>
+        <h3>Joyce Liao</h3>
+        <p>Software Developer</p>
+        <ul className={classes.NavigationItems}>
+            <li><a>Home</a></li>
+            <li><a>Python</a></li>
+            <li><a>JavaScript</a></li>
+            <li><a>Algorithm</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </div>
 );
 
 export default navigationItems;
