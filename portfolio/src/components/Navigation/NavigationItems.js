e10808afa3eae8c6c0081a82fffc1ba312b937e6
Joyce Liao <joyliao07@gmail.com>
2019-06-27 23:39:31 -0700

    create navigationItems inside landing

--- portfolio/src/components/Navigation/NavigationItems.js
+++ portfolio/src/components/Navigation/NavigationItems.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import classes from './NavigationItems.css';
 
 const navigationItems = (props) => (
-    <p>Navigation Items</p>
+    <ul className={classes.NavigationItems}>
+        <li><a>Home</a></li>
+        <li><a>Python</a></li>
+        <li><a>JavaScript</a></li>
+        <li><a>Algorithm</a></li>
+        <li><a>About</a></li>
+        <li><a>Contact</a></li>
+    </ul>
 );
 
 export default navigationItems;
--- portfolio/src/containers/Landing/Landing.js
+++ portfolio/src/containers/Landing/Landing.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import NavigationItems from '../../components/Navigation/NavigationItems';
+import classes from './Landing.css';
 
 class Landing extends Component {
     render () {
         return (
-            <div>
+            <div className={classes.Landing}>
                 This is landing page
+                <NavigationItems/>
             </div>
         );
     }
